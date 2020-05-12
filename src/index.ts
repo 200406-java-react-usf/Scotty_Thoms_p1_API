be9154d397f44930650a23cce1341b99575451f6
Scotty Thoms <spthoms19@gmail.com>
2020-05-11 21:34:20 -0500

    setting up basic endpoint to test with pipeline

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import dotenv from 'dotenv';
+import express from 'express';
+import fs from 'fs';
+import morgan from 'morgan';
+import path from 'path';
+
+const app = express();
+
+app.use('/', express.json());
+
+app.listen(8080, () => {
+    console.log(`Application up and running.`);
+});
