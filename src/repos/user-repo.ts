876f95b990ec721bf03a49b5b08bad61172b0298
Scotty Thoms <spthoms19@gmail.com>
2020-05-14 15:48:46 -0500

    changed middleware to work with s3 bucket

--- src/repos/user-repo.ts
+++ src/repos/user-repo.ts
@@ -18,6 +18,10 @@ export class UserRepository {
     on u.user_role_id = ur.role_id
     `;
 
+    /**
+     * Gets all users from database
+     * Needs admin access (not implemented yet)
+     */
     async getAll(): Promise<User[]> {
         let client: PoolClient;
         try {
@@ -25,6 +29,33 @@ export class UserRepository {
             let sql = `${this.baseQuery} order by u.user_id`;
             let rs = await client.query(sql);
             return rs.rows;
+        } catch (e) {
+            throw new InternalServerError();
+        } finally {
+            client && client.release();
+        }
+    }
+
+    /**
+     * Creates a new user in the system with info provided.
+     * @param newUser {User} the new user that will be added to the database
+     */
+    async save(newUser: User): Promise<User> {
+        let client: PoolClient;
+
+        try {
+            client = await connectionPool.connect();
+            let roleId = (await client.query('select user_id from user_roles where name = $1', [newUser.role])).rows[0].id;
+
+            let sql = `
+                insert into users (username, password, first_name, last_name, email, user_role_id)
+                values ($1, $2, $3, $4, $5) returning user_id
+            `;
+
+            let rs = await client.query(sql, [newUser.username, newUser.password, newUser.firstName, newUser.lastName, newUser.email, roleId])
+            newUser.id = rs.rows[0].id;
+
+            return newUser;
         } catch (e) {
             throw new InternalServerError();
         } finally {
