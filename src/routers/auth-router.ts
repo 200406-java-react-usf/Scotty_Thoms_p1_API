9c2baed0e4db2c4bb61f8e0d125d4e448bf08fbf
Scotty Thoms <spthoms19@gmail.com>
2020-05-18 09:54:55 -0500

    added auth-router

---
+++ src/routers/auth-router.ts
@@ -0,0 +1,32 @@
+import express, { response } from 'express';
+import AppConfig from '../config/app';
+import { Principal } from '../dtos/principal';
+
+export const AuthRouter = express.Router();
+
+const userService = AppConfig.userService;
+
+/**
+ * Used to "logout"
+ */
+
+ AuthRouter.get('', async (req,resp) => {
+     delete req.session.principal;
+     resp.status(204).send();
+ });
+
+/**
+ * Used to login and will check to see if user is "admin"
+ */
+
+AuthRouter.post('', async (req,resp) => {
+    try {
+        const { username, password } = req.body;
+        let authUser = await userService.authenticateUser(username, password);
+        let payload = new Principal(authUser.id, authUser.username, authUser.role);
+        req.session.principal = payload;
+        resp.status(200).json(payload);
+    } catch (e) {
+        resp.status(e.statusCode || 500).json(e);
+    }
+});
